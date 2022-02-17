e5f29183750c0d1e23843f62c0b1d424459b7c97
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-02-16 23:42:13 -0300

    styles -> landing + dev pages

---
+++ src/styles/components/deletemodal.ts
@@ -0,0 +1,16 @@
+export const ReactDeleteModalStyles = {
+    overlay: {
+        backgroundColor: 'hsla(0, 1%, 20%, 0.7)',
+        zIndex: 2
+    },
+    content: {
+        color: '#fff',
+        margin: 'auto',
+        maxHeight: '250px',
+        maxWidth: '460px',
+        border: '0',
+        padding: '0',
+        borderRadius: '10px',
+        backgroundColor: 'hsla(0, 1%, 20%, 0.95)'
+    }
+}
---
+++ src/styles/components/devsmodal.ts
@@ -0,0 +1,16 @@
+export const ReactModalStyles = {
+    overlay: {
+        backgroundColor: 'hsla(0, 1%, 20%, 0.7)',
+        zIndex: 2
+    },
+    content: {
+        color: '#fff',
+        margin: 'auto',
+        maxHeight: '650px',
+        maxWidth: '470px',
+        border: '0',
+        padding: '0',
+        borderRadius: '10px',
+        backgroundColor: 'hsla(0, 1%, 20%, 0.95)'
+    }
+}
