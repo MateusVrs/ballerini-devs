b0198c1506a2fed73aacdcddcda69e75c83d4f60
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-02-16 23:41:15 -0300

    general header + button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,13 @@ import { ButtonHTMLAttributes } from 'react'
 
 import '../styles/components/button.scss'
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    className?: string;
+}
 
-export function Button(props: ButtonProps) {
+export function Button({className = '', ...props}: ButtonProps) {
 
     return (
-        <button {...props} className="button" />
+        <button {...props} className={`button ${className}`} />
     )
 }
---
+++ src/components/HeaderBase.tsx
@@ -0,0 +1,33 @@
+import linkedinLogo from '../assets/images/linkedin.svg'
+import facebookLogo from '../assets/images/facebook.svg'
+import discordLogo from '../assets/images/discord.svg'
+import balleriniDevsLogo from '../assets/images/logo.svg'
+
+import '../styles/components/headerbase.scss'
+import { Link } from "react-router-dom";
+
+import { HeaderBaseProps } from "../types/headerbase";
+
+export function HeaderBase({ children }: HeaderBaseProps) {
+    return (
+        <header id="header-base">
+            <div className="social-container">
+                <a href="https://www.linkedin.com/company/comunidadeballerini/" target="_blank" rel="noreferrer">
+                    <img src={linkedinLogo} alt="linkedin logo" />
+                </a>
+                <a href="https://www.facebook.com/rafaella.balleriniribeirogomes" target="_blank" rel="noreferrer">
+                    <img src={facebookLogo} alt="facebook logo" />
+                </a>
+                <a href="https://discord.gg/wagxzStdcR" target="_blank" rel="noreferrer">
+                    <img src={discordLogo} alt="discord logo" />
+                </a>
+            </div>
+            <div className="title-container">
+                <img src={balleriniDevsLogo} alt="ballerini devs logo" />
+                <h1><Link to='/'>Ballerini Devs</Link></h1>
+                <div className="void"></div>
+            </div>
+            {children}
+        </header>
+    )
+}
