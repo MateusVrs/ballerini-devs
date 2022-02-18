730cc13df943a19fb01e30f4dfb7c6b5ecad49ac
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-02-18 04:56:19 -0300

    index structure -> scss/providers/routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { DevsModalContextProvider } from "./contexts/DevsModalContext";
+import { LoandingContextProvider } from "./contexts/LoadingContext";
+
+import { ChatPage } from "./pages/ChatPage";
 import { DevsPage } from "./pages/DevsPage";
 import { LandingPage } from "./pages/LandingPage";
 
 export function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/devs" element={<DevsPage />} />
-        </Routes>
-      </BrowserRouter>
+    <LoandingContextProvider>
+      <AuthContextProvider>
+        <DevsModalContextProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/devs" element={<DevsPage />} />
+              <Route path="/chat" element={<ChatPage />} />
+            </Routes>
+          </BrowserRouter>
+        </DevsModalContextProvider>
+      </AuthContextProvider>
+    </LoandingContextProvider>
   )
 }
